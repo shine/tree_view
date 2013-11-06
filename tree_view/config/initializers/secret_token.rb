9bdf08a8fb34a30ae52112f94d943edfd27723bc
Victor Brylew <victor.brylew@gmail.com>
2013-11-06 15:30:16 +0100

    initial version

---
+++ tree_view/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreeView::Application.config.secret_token = '7fde972ef72ff6208ffbadc044c05e435e565ff1acc8bdcd7a6ffe84ad8e899461edd9bcdab2eeed8558d66f194492616c21bf2d84bf1b3a15837d1410c46c2b'
